8d46423a5b82a41ae399ea21e9e6981a8704f500
Jerry Lau <jerryzlau@gmail.com>
2018-02-25 21:38:02 -0800

    relearning how to calculate pearson coorelation coefficient

---
+++ README.md
@@ -0,0 +1,15 @@
+# Notes
+
+## Terms
+
+1. __Collaborative Filtering__ - works by searching a large group of people
+and finding a smaller set with tastes similar to yours
+
+2. __Euclidean Distance Score__ - Calculates the distance between two plot point as similarity score. Not good for data that are not nomalized.
+
+3. __Pearson Correlation Score__ - Correlation coefficient is a measure
+of how well two sets of data fit on a straight line (best-fit line).
+
+4. __Best-Fit Line__ - Straight line diagonal to X-axis and Y-axis.
+
+5. __Grade Inflation__ - Basically a curve for average score
--- recommendations.py
+++ recommendations.py
@@ -50,3 +50,66 @@ critics = {
     'Toby': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0,
              'Superman Returns': 4.0},
 }
+
+# Returns a similarity score of person 1 and person 2
+
+# This is the Euclidean Distance Score approach
+# Calculates the distance between two plot point as similarity score
+def sim_distance(prefs, p1, p2):
+
+  # get shared items
+  shared_items = {}
+  for item in prefs[p1]:
+    if item in prefs[p2]:
+      shared_items[item]=1
+  
+  # return 0 if no items match
+  if len(shared_items)==0:
+    return 0
+  
+  # otherwise, find the distance between the two plot points
+  sum_of_squares=sum([pow(prefs[p1][item]-prefs[p2][item],2) for item in shared_items])
+
+  # my python beginner way to calculate sum of square
+  total = 0
+  for item in shared_items:
+    total += pow(prefs[p1][item]-prefs[p2][item],2)
+
+  return 1/(1+sqrt(sum_of_squares))
+
+# Returns Pearson correlation coefficient for person1 and person2
+def sim_pearson(prefs, p1, p2)
+    # get shared items
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item]=1
+
+    # Find number of elements
+    n = len(si)
+
+    # Return 0 if nothing in common
+    if n == 0: return 0
+
+    # Add up all the preferences
+    sum1=sum([prefs[p1][item] for item in si])
+    sum2=sum([prefs[p2][item] for item in si])
+
+    # sum all the squares
+    sum1Sq=sum([pow(prefs[p1][item],2) for item in si])
+    sum2Sq=sum([pow(prefs[p2][item],2) for item in si])
+
+    # sum the products
+    pSum=sum([prefs[p1][item]*prefs[p2][item] for item in si])
+
+    # Calculate pearson score
+    num=pSum-(sum1*sum2/n)
+    den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+    if den==0: return 0
+
+    r=num/den
+
+    return r
+
+
+
